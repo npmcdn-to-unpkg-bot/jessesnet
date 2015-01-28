9ebea6e4f8716eff57a892157257737ac92fe28e
Jesse Cascio <jessecascio@gmail.com>
2015-01-27 20:45:43 -0700

    example list, template link, find limit

--- development-notes/nodejs-mongodb/find.js
+++ development-notes/nodejs-mongodb/find.js
@@ -5,11 +5,22 @@ MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
 
     var query = { 'State' : 'Florida' };
 
-    db.collection('data').find(query).toArray(function(err, docs) {
+    db.collection('data').find(query).limit(1).toArray(function(err, docs) {
+        if(err) throw err;
+
+        console.dir(docs);
+
+        // db.close();
+    });
+
+    var query = { 'State' : 'California' };
+ 
+    db.collection('data').find(query).limit(1).toArray(function(err, docs) {
         if(err) throw err;
 
         console.dir(docs);
 
         db.close();
     });
+
 });
--- development-notes/nodejs-mongodb/template.js
+++ development-notes/nodejs-mongodb/template.js
@@ -1,3 +1,5 @@
+// http://mongodb.github.io/node-mongodb-native/2.0/overview/quickstart/
+
 var MongoClient = require('mongodb').MongoClient;
 
 MongoClient.connect('mongodb://localhost:27017/weather', function(err, db) {
