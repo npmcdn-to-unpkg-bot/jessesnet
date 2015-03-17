e268197548e0183d964389e56ab094b4b11ded8b
Jesse Cascio <jessecascio@gmail.com>
2015-03-16 19:09:29 -0700

    class, template, and namespace c++ examples

---
+++ portfolio/data-structures/linked-list/c++/linkedlist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+// g++ -std=c++11 intro.cpp -o name
+// ./name
+
+/*
+	Task - replace each space with a %20
+*/
+
+struct Item {
+
+};
+
+struct List {
+	Item *next;
+
+	void add(Item item) {
+		*next = item;
+	}
+};
+
+int main()
+{
+	
+}
+
