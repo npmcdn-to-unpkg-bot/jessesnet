d10502c2d8d0f694cbfc2258818537c318c8db01
Jesse Cascio <jessecascio@gmail.com>
2015-03-17 20:00:13 -0700

    c++ linked list updates

--- portfolio/data-structures/linked-list/c++/LinkedList.h
+++ portfolio/data-structures/linked-list/c++/LinkedList.h
@@ -10,42 +10,136 @@ template <class T>
 class LinkedList 
 {
 	Node<T> *current = NULL;
-	Node<T> *head = NULL;
-	Node<T> *tail = NULL;
+	Node<T> *head = NULL; // last, freshest, element
+	Node<T> *tail = NULL; // first, oldest, element
+
+	int listsize = 0;
 
 	// each position stores an element
 	// positions are nodes
 	public:
-		void insert(T element);
-		T element();
-		T first(); // probably needs to be pointers, returns positions, first().element()
-		int last();
-		int before(int position);
-		int after(int position);
-		bool isEmpty();
+		// LinkedList<T>* first(); // can return this
+		// LinkedList<T>* last();
+   		
+   		bool isEmpty();
 		int size();
-		void insertBefore(int position, T element); // should these be after nodes ???
-		void insertAfter(int position, T element);
-		void remove(int position);
-		void set(int position, T element);
+   		T first();
+   		T last();
+		void insertFirst(T element);
+		void insertLast(T element);
+		void removeFirst();
+		void removeLast();
+		
+		void test();
+
+		// int before(int position);
+		// int after(int position);
+		// void insertBefore(int position, T element); // should these be after nodes ???
+		// void insertAfter(int position, T element); // after a node, insert this node
 };
 
 template <class T>
-void LinkedList<T>::insert(T data)
+bool LinkedList<T>::isEmpty()
 {
-	Node<T> element;
-	element.set(data);
-
-	element.next = NULL;
-	
-	current = &element;
-	head    = &element;
-
-	if (!tail) {
-		tail = &element;
-	}
+	return this->listsize == 0 ? true : false;
 }
 
+template <class T>
+int LinkedList<T>::size()
+{
+	return this->listsize;	
+}
+
+template <class T>
+T LinkedList<T>::first()
+{
+	if (!this->tail) {
+		throw false;
+	}
+
+	return this->tail->read();
+}
+
+template <class T>
+void LinkedList<T>::insertFirst(T data)
+{
+	Node<T> *element = new Node<T>;
+	
+	element->set(data);
+	element->next = tail;
+
+	this->current = element;
+	this->tail    = element;
+	
+	if (!this->head) {
+		this->head = element;
+	}
+
+	this->listsize++;
+
+	// investigate implication of needing to delete pointer
+	// delete element;
+}
+
+template <class T>
+T LinkedList<T>::last()
+{
+	if (!this->head) {
+		throw false;
+	}
+
+	return this->head->read();
+}
+
+template <class T>
+void LinkedList<T>::insertLast(T data)
+{
+	Node<T> *element = new Node<T>;
+	
+	element->set(data);
+	element->next = NULL;
+
+	this->head->next = element;
+
+	this->current = element;
+	this->head    = element;
+	
+	this->listsize++;
+
+	// investigate implication of needing to delete pointer
+	// delete element;
+}
+
+template <class T>
+void LinkedList<T>::test()
+{
+	std::cout << this->tail->read() << "\n";
+
+	Node<T> *next1 = this->tail->next;
+	std::cout << next1->read() << "\n";
+
+	Node<T> *next2 = next1->next;
+	std::cout << next2->read() << "\n";
+
+	Node<T> *next3 = next2->next;
+	std::cout << next3->read() << "\n";
+
+	Node<T> *next4 = next3->next;
+	std::cout << next4->read() << "\n";
+
+	Node<T> *next5 = next4->next;
+	std::cout << next5->read() << "\n";
+
+	/*
+	for (int i=0; i<this->listsize; i++) {
+		Node<T> *next = this->tail->next;
+		std::cout << next->read() << "\n";
+		delete next;
+	}
+	*/
+}
+
+/*
 template <class T>
 T LinkedList<T>::element()
 {
@@ -53,6 +147,51 @@ T LinkedList<T>::element()
 	return (*current).read();
 }
 
+template <class T>
+LinkedList<T>* LinkedList<T>::first()
+{
+	if (!tail) {
+		throw false;
+	}
+
+	current = tail;
+	return this;
 }
+
+template <class T>
+LinkedList<T>* LinkedList<T>::last()
+{
+	if (!head) {
+		throw false;
+	}
+
+	current = head;
+	return this;
+}
+
+template <class T>
+void LinkedList<T>::insert(T data)
+{
+	Node<T> *element = new Node<T>;
+	
+	element->set(data);
+	element->next = NULL;
+
+	this->current = element;
+	this->head    = element;
+	
+	if (!tail) {
+		this->tail = element;
+	}
+
+	this->listsize++;
+
+	// investigate implication of needing to delete pointer
+	// delete element;
+}
+*/
+
+}
+
 
 #endif
--- portfolio/data-structures/linked-list/c++/run.cpp
+++ portfolio/data-structures/linked-list/c++/run.cpp
@@ -8,8 +8,22 @@ using namespace std;
 int main()
 {
 	LinkedList<int> list;
-	list.insert(1);
 
-	cout << list.element() << "\n";
+	list.insertFirst(1);
+	list.insertFirst(12);
+	list.insertFirst(14);
+	list.insertFirst(23);
+	list.insertFirst(2183);
+	list.insertFirst(2938);
+	
+	/*
+	cout << "SIZE : " << list.size() << "\n";
+	
+	cout << list.first() << "\n";
+	cout << list.last() << "\n";
+	*/
+
+	cout << "TEST\n";
+	list.test();	
 }
 
