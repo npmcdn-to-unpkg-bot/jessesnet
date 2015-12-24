010bd962f85b13e5ab53b3125f710d12de36dc6c
Jesse Cascio <jesse.cascio@timeinc.com>
2015-12-23 16:44:07 -0800

    event notes

---
+++ development-notes/nodejs-event/run.js
@@ -0,0 +1,31 @@
+
+/*
+  for (var i=0; i<5000000000; i++) { }
+
+  process.nextTick (function (){          
+    emitter.emit('poot', {foo:'baz'});
+  });
+*/
+
+let events  = require('events');
+let emitter = new events.EventEmitter();
+
+// multiple
+emitter.on('poot', (data) => {
+  console.log('pootin....', data);
+  // blocks
+});
+
+emitter.emit('poot', {foo:'baz'});
+emitter.emit('poot', {foo:'baz'});
+
+// single
+emitter.once('foop', (data) => {
+  console.log('foopin....', data);
+});
+
+emitter.emit('foop', {foo:'baz'});
+emitter.emit('foop', {foo:'baz'});
+
+console.log(emitter.listenerCount('poot'));
+console.log(emitter.listenerCount('foop'));
