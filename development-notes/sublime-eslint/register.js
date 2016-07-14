c41bdee0e77ee196df31de702a9bd5c0967a61a6
Jesse Cascio <jesse.cascio@timeinc.com>
2016-07-13 17:54:34 -0700

    babel, hoist

---
+++ development-notes/sublime-eslint/register.js
@@ -0,0 +1,8 @@
+
+// https://github.com/thejameskyle/babel-handbook/blob/master/translations/en/user-handbook.md
+// http://babeljs.io/docs/plugins/
+
+require("babel-register");
+require("babel-polyfill");
+
+require("./index.js");
