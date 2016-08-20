0986da964389e3d20a05362e6b1cb9a02cd08e61
Jesse Cascio <jesse.cascio@timeinc.com>
2016-08-20 15:29:22 -0700

    dir restructure

--- development-notes/sublime-eslint/register.js
+++
@@ -1,8 +0,0 @@
-
-// https://github.com/thejameskyle/babel-handbook/blob/master/translations/en/user-handbook.md
-// http://babeljs.io/docs/plugins/
-
-require("babel-register");
-require("babel-polyfill");
-
-require("./index.js");
